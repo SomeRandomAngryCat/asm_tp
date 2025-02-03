337a0be0078d199d267fc46d6ca6682b3445db7c
SomeRandomAngryCat <migijo@proton.me>
2025-02-04 00:46:21 +0100

    asm04.s

--- asm04/asm04.s
+++ asm04/asm04.s
@@ -1,40 +1,61 @@
-section .bss
-    number resb 10
-
 section .text
     global _start
 
 _start:
-
-    mov rax, 0
-    mov rdi, 0
-    mov rsi, number
-    mov rdx, 10
-    syscall
-
-    mov rbx, 0
-    mov rdi, number
-
-convert_ascii:
-    movzx rax, byte [rdi]
-    cmp rax, 10
-    je check_parity
-    sub rax, '0'
-    imul rbx, rbx, 10
-    add rbx, rax
-    inc rdi
-    jmp convert_ascii
-
-check_parity:
-    test rbx, 1
-    jz return_0
-
-return_1:
-    mov rax, 60
+    mov rdi, [rsp]
+    cmp rdi, 2
+    jne exit_1
+    
+    mov rsi, [rsp+16]
+    call validate_number
+    cmp rax, 0
+    jne exit_2
+    
+    mov rax, [rsi]
+    cmp word [rsi], 0x3234
+    jne exit_1
+    cmp byte [rsi+2], 0
+    jne exit_1
+    
+    mov rax, 1
     mov rdi, 1
+    mov rsi, msg
+    mov rdx, msg_len
+    syscall
+    
+    xor rdi, rdi
+    jmp exit
+
+validate_number:
+    mov rcx, 0
+check_loop:
+    mov al, [rsi+rcx]
+    test al, al
+    jz valid
+    cmp al, '0'
+    jb invalid
+    cmp al, '9'
+    ja invalid
+    inc rcx
+    jmp check_loop
+invalid:
+    mov rax, 1
+    ret
+valid:
+    xor rax, rax
+    ret
+
+exit_2:
+    mov rdi, 2
+    jmp exit
+
+exit_1:
+    mov rdi, 1
+    
+exit:
+    mov rax, 60
     syscall
 
-return_0:
-    mov rax, 60
-    xor rdi, rdi
-    syscall
+section .data
+    msg db "1337\n"
+    msg_len equ $ - msg
