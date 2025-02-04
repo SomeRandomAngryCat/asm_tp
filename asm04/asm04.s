1bb2da99a7d97b9f057703f3e0c552a3649704b5
SomeRandomAngryCat <migijo@proton.me>
2025-02-04 01:02:21 +0100

    asm04.s

--- asm04/asm04.s
+++ asm04/asm04.s
@@ -1,66 +1,40 @@
+section .bss
+    number resb 10
+
 section .text
     global _start
 
 _start:
-    mov rdi, [rsp]
-    cmp rdi, 2
-    jne exit_1
-    
-    mov rsi, [rsp+16]
-    call validate_number
-    cmp rax, 0
-    jne exit_2
-    
-    mov rax, [rsi]
-    cmp word [rsi], 0x3234
-    jne exit_1
-    cmp byte [rsi+2], 0
-    jne exit_1
-    
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, msg
-    mov rdx, msg_len
+
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, number
+    mov rdx, 10
     syscall
-    
-    xor rdi, rdi
-    jmp exit
 
-validate_number:
-    mov rcx, 0
-    mov rdx, 0
-check_loop:
-    mov al, [rsi+rcx]
-    test al, al
-    jz valid
-    cmp al, '0'
-    jb invalid_input
-    cmp al, '9'
-    ja invalid_input
-    inc rcx
-    inc rdx
-    jmp check_loop
-invalid_input:
-    mov rax, 1
-    mov rdi, 2
-    jmp exit
-valid:
-    cmp rdx, 0
-    je invalid_input
-    xor rax, rax
-    ret
+    mov rbx, 0
+    mov rdi, number
 
-exit_2:
-    mov rdi, 2
-    jmp exit
+convert_ascii:
+    movzx rax, byte [rdi]
+    cmp rax, 10
+    je check_parity
+    sub rax, '0'
+    imul rbx, rbx, 10
+    add rbx, rax
+    inc rdi
+    jmp convert_ascii
 
-exit_1:
-    mov rdi, 1
-    
-exit:
+check_parity:
+    test rbx, 1
+    jz return_0
+
+return_1:
     mov rax, 60
+    mov rdi, 1
     syscall
 
-section .data
-    msg db "1337\n"
-    msg_len equ $ - msg
+return_0:
+    mov rax, 60
+    xor rdi, rdi
+    syscall
