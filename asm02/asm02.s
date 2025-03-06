e499ffbcc6298199b8bff3254a9045119e62c7ca
SomeRandomAngryCat <migijo@proton.me>
2025-03-07 00:46:09 +0100

    Update asm02.s

--- asm02/asm02.s
+++ asm02/asm02.s
@@ -1,50 +1,50 @@
 section .data
-    msg db "1337", 10      ; Message à afficher (1337) suivi d'un saut de ligne
-    msg_len equ $ - msg    ; Longueur du message
-    
-    input_val db "42"      ; Valeur attendue en entrée
-    input_len equ $ - input_val ; Longueur de la valeur attendue
-
-section .bss
-    buffer resb 16         ; Buffer pour stocker l'entrée utilisateur
+    message db "1337", 10    ; 10 est le code ASCII pour le saut de ligne
+    message_len equ $ - message
 
 section .text
     global _start
 
 _start:
-    ; Lire l'entrée utilisateur
-    mov rax, 0          ; syscall read
-    mov rdi, 0          ; stdin
-    mov rsi, buffer     ; buffer où stocker l'entrée
-    mov rdx, 16         ; taille maximale à lire
+    ; Récupérer le nombre d'arguments
+    pop rdi         ; Le nombre d'arguments est placé en haut de la pile
+    
+    ; Vérifier s'il y a au moins un argument (en plus du nom du programme)
+    cmp rdi, 2
+    jl exit_with_error    ; S'il n'y a pas assez d'arguments, on quitte avec une erreur
+    
+    ; Récupérer le pointeur vers le premier argument
+    pop rdi         ; On retire le nom du programme (argv[0])
+    pop rdi         ; Le premier argument est maintenant dans rdi (argv[1])
+    
+    ; Vérifier si l'argument est "42"
+    mov al, [rdi]
+    cmp al, '4'
+    jne exit_with_error
+    
+    mov al, [rdi+1]
+    cmp al, '2'
+    jne exit_with_error
+    
+    ; Vérifier que la chaîne se termine ici (pas d'autres caractères)
+    mov al, [rdi+2]
+    test al, al
+    jnz exit_with_error
+    
+    ; Afficher "1337"
+    mov rax, 1      ; syscall number pour write
+    mov rdi, 1      ; file descriptor: STDOUT
+    mov rsi, message
+    mov rdx, message_len
     syscall
-
-    ; Vérifier si l'entrée est valide
-    cmp rax, input_len  ; Comparer la longueur lue avec la longueur attendue
-    jne exit_failure    ; Si différent, sortir avec échec
-
-    ; Comparer l'entrée avec "42"
-    mov rcx, input_len
-    mov rsi, buffer
-    mov rdi, input_val
-    repe cmpsb          ; Comparer les chaînes octet par octet
-    jne exit_failure    ; Si différent, sortir avec échec
-
-    ; Afficher "1337" si l'entrée est "42"
-    mov rax, 1          ; syscall write
-    mov rdi, 1          ; stdout
-    mov rsi, msg        ; message à afficher
-    mov rdx, msg_len    ; longueur du message
+    
+    ; Quitter avec le code de retour 0
+    mov rax, 60     ; syscall number pour exit
+    xor rdi, rdi    ; code de retour 0
     syscall
-
-exit_success:
-    ; Retourner 0 (succès)
-    mov rax, 60         ; syscall exit
-    mov rdi, 0          ; code de retour 0 (succès)
-    syscall
-
-exit_failure:
-    ; Retourner 1 (échec)
-    mov rax, 60         ; syscall exit
-    mov rdi, 1          ; code de retour 1 (échec)
+    
+exit_with_error:
+    ; Quitter avec le code de retour 1
+    mov rax, 60     ; syscall number pour exit
+    mov rdi, 1      ; code de retour 1
     syscall
